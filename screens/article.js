742f088a666a415412cb2bc1c204fc04240757e2
rei234555 <reinantosaputra@gmail.com>
2024-01-07 23:02:16 +0700

    UI article terbaaru

--- screens/article.js
+++ screens/article.js
@@ -1,4 +1,4 @@
-import { Heading, Image, Text, FlatList, Box} from "native-base";
+import { Heading, Image, Text, FlatList, Box, ScrollView, VStack} from "native-base";
 import { TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Header } from "../components";
@@ -8,14 +8,19 @@ const Article = () => {
   const navigation = useNavigation();
   const renderitem = ({ item }) => {
     return (
+
         <TouchableOpacity
         activeOpacity={0.5}
         onPress={() => navigation.navigate("Berita Detail", { item: item })}
         >
+
         <Box
-          p={"4"}
+          alignSelf={"center"}
+          w={"100%"}
+          mt={2}
+          p={"3"}
           borderBottomColor={"coolGray.600"}
-          borderBottomWidth={2}
+          borderBottomWidth={0}
           flexDirection="row"
           flex={1} 
           bg={"#C4E9F5"}>
@@ -42,13 +47,119 @@ const Article = () => {
   return (
     <>
       <Header title={"Article Mentalife"} withBack={true}  />
+
+      <ScrollView>
+
+      <Box alignSelf={"flex-start"} ml={3} mt={2} w={"90%"}>
+        <Text fontSize={"23"} fontWeight={"semibold"}>
+          Jelajahi Article Mentalife
+        </Text>
+      </Box>
+
+      <Box alignSelf={"flex-start"} ml={3} mt={2} w={"95%"}>
+        <Text fontSize={"14"} fontWeight={"normal"}>
+          Kumpulan tips kesehatan, informasi penyakit dan kesehatan
+          yang lengkap dan terpercaya Article Mentalife
+        </Text>
+      </Box>
+
+      <Box py={"3"} bg={"#C4E9F5"} mt={5}>
+              <Text bold fontSize={20} w={"300"} ml={6} >Berita yang sedang hangat</Text>
+              <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+
+                      <Box w={"48"} mr={"4"} mt={4}>
+                        <Image
+                          source={{ uri: 'https://i.pinimg.com/564x/e4/13/41/e413416f6d8e877798c98a028551ae64.jpg'}}
+                          alignSelf={"center"}
+                          size={"xl"}
+                          alt="Image Data"
+                          mb={"2"}
+                          borderRadius={10}
+                          w={"90%"}
+                        />
+                        <Heading
+                          fontSize={"15"}
+                          lineHeight={"xs"}
+                          ellipsizeMode="tail"
+                          numberOfLines={1}
+                          bold
+                          alignSelf={"center"}
+                        >
+                            Pentingnya Mental Health
+                        </Heading>
+                        <Text fontSize={"xs"} color="black" ml={3} numberOfLines={1}>
+                         Banyaknya generasi muda yang mulai mengalami depresi
+                        </Text>
+                      </Box>
+
+                      <Box w={"48"} mr={"4"} mt={4}>
+                        <Image
+                          source={{ uri: 'https://i.pinimg.com/564x/f6/ce/8c/f6ce8c916f0f9a8e03873c24269cd5d8.jpg'}}
+                          alignSelf={"center"}
+                          size={"xl"}
+                          alt="Image Data"
+                          mb={"2"}
+                          borderRadius={10}
+                          w={"90%"}
+                        />
+                        <Heading
+                          fontSize={"15"}
+                          lineHeight={"xs"}
+                          ellipsizeMode="tail"
+                          numberOfLines={1}
+                          bold
+                          alignSelf={"center"}
+                        >
+                          Tekanan Pekerjaan
+                        </Heading>
+                        <Text fontSize={"xs"} color="black" ml={3} numberOfLines={1}>
+                         Pentingnya kesehatan mental untuk pegawai
+                        </Text>
+                      </Box>
+
+                      <Box w={"48"} mr={"4"} mt={4}>
+                        <Image
+                          source={{ uri: 'https://i.pinimg.com/564x/ed/f4/fa/edf4fa1b13abdd338bceb1f066b43af7.jpg'}}
+                          alignSelf={"center"}
+                          size={"xl"}
+                          alt="Image Data"
+                          mb={"2"}
+                          borderRadius={10}
+                          w={"90%"}
+                        />
+                        <Heading
+                          fontSize={"15"}
+                          lineHeight={"xs"}
+                          ellipsizeMode="tail"
+                          numberOfLines={1}
+                          bold
+                          alignSelf={"center"}
+                        >
+                            Lingkungan kerja yang Toxic
+                        </Heading>
+                        <Text fontSize={"xs"} color="black" ml={3} numberOfLines={1}>
+                         Belum tentu ketika anda bekerja mendapatkan lingkungan kerja yang menyenangkan
+                        </Text>
+                      </Box>
+
+              </ScrollView>
+            </Box>
+
+            <Box alignSelf={"flex-start"} ml={3} mt={6} w={"90%"}>
+        <Text fontSize={"23"} fontWeight={"semibold"}>
+          Daftar Article Mentalife
+        </Text>
+      </Box>
+
       <FlatList
         data={databerita}
         renderItem={renderitem}
         keyExtractor={(item) => item.id}
         showsVerticalScrollIndicator={false}
       />
+      </ScrollView>
     </>
+    
   );
 };
 
